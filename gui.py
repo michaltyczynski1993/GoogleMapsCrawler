bdec55e2f4e50b1f5f45f4f56aa1c1ad1025f16d
Michał <m.tyczynski1993@gmail.com>
2022-02-22 20:42:13 +0100

    gui changes - font size, button size

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from pydoc import text
 from tkinter import *
+from turtle import width
 
 class Gui(Frame):
     
@@ -10,16 +11,17 @@ class Gui(Frame):
 
     def create_widgets(self):
         self.label = Label(self, text = 'Enter Your Keywords')
-        self.label.pack()
+        self.label.config(font = ('Arial', 15))
+        self.label.pack(pady = 20)
 
         self.entry = Entry(self)
-        self.entry.pack()
+        self.entry.pack(pady= 10, ipadx=40)
 
-        self.button = Button(self, text = 'Submit')
-        self.button.pack(side = 'bottom')
+        self.button = Button(self, text = 'Submit', width = 20)
+        self.button.pack(side = 'bottom', pady = 10)
 
 root = Tk()
 root.title('Google Maps Searcher')
-root.geometry('300x150')
+root.geometry('300x200')
 app = Gui(root)
 root.mainloop()
