b87680ad849e6c6b5aac1477631a1b118648d654
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-08-24 16:31:14 +0200

    save links in list and locate region first

--- googlescraper.py
+++ googlescraper.py
@@ -1,3 +1,4 @@
+from re import search
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,12 +10,16 @@ from selenium.webdriver.chrome.service import Service
 import time
 import locators
 import timeit
-
 #setup
+search_region = input('enter searching region: ')
+search_data = input('enter keywords: ')
+search_links = []
+
 options = Options()
 # options.add_argument("--headless")
 driver = webdriver.Chrome(service=Service('C:\TestFiles\chromedriver.exe'), options=options)
 driver.implicitly_wait(10)
+
 # go to google maps main page and check for localization popup
 start = timeit.default_timer()
 driver.get('https://www.google.pl/maps/preview')
@@ -24,25 +29,45 @@ try:
 except:
     print('button is not clickable')
 
+# first data input for locating searching region
+user_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locators.SEARCH_INPUT))
+user_input.send_keys(search_region)
+submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locators.SEARCH_BUTTON))
+submit_button.click()
+time.sleep(8)
+
 # take string to search in google maps results
 user_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locators.SEARCH_INPUT))
-user_input.send_keys('sklep warszawa')
-# time.sleep(3)
-# submit_button = self.driver.find_element(*locators.SEARCH_BUTTON) 
-submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(locators.SEARCH_BUTTON))
+user_input.clear()
+user_input.send_keys(search_data)
 submit_button.click()
 
 # scrolling down all found google maps results
 results_container = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locators.RESULTS_CONTAINER))
-# verical_ordinate = 100
 while True:
     # Get scroll height
     last_height = driver.execute_script("return arguments[0].scrollHeight", results_container)
     driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", results_container)
-    # verical_ordinate += 300
-    time.sleep(2)
+    time.sleep(3)
+
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return arguments[0].scrollHeight", results_container)
     if new_height == last_height:
         break
     last_height = new_height
+
+# find all results 
+results = driver.find_elements(*locators.RESULTS)
+
+# find links for every result
+for result in results:
+    link = result.get_attribute('href')
+    search_links.append(link)
+
+# print the found links (to gmaps sites)
+for link in search_links:
+    print(link)
+print(len(search_links))
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)
